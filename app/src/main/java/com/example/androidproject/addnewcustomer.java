8629790214b2918fc8b1a7494bf14b1ce23de2b9
mohamadhajali <mohamad184aliq@gmail.com>
2022-01-07 23:39:14 +0200

    1.2

--- app/src/main/java/com/example/androidproject/addnewcustomer.java
+++ app/src/main/java/com/example/androidproject/addnewcustomer.java
@@ -1,12 +1,24 @@
 package com.example.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 
 public class addnewcustomer extends AppCompatActivity {
         EditText fullname;
@@ -24,14 +36,109 @@ public class addnewcustomer extends AppCompatActivity {
         username = findViewById(R.id.username);
         phonenumber = findViewById(R.id.phonenumber);
         save = findViewById(R.id.savecustmer);
-        save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                saveOnDataBase();
-            }
-        });
+
     }
-    public void saveOnDataBase(){
+    private String processRequest(String restUrl) throws UnsupportedEncodingException {
+        String userName  = username.getText().toString();
+        String fullName  = fullname.getText().toString();
+        String phoneNum  = phonenumber.getText().toString();
+
+        String data = URLEncoder.encode("userName", "UTF-8")
+                + "=" + URLEncoder.encode(userName, "UTF-8");
+
+        data += "&" + URLEncoder.encode("fullName", "UTF-8") + "="
+                + URLEncoder.encode(fullName, "UTF-8");
+
+        data += "&" + URLEncoder.encode("phoneNum", "UTF-8")
+                + "=" + URLEncoder.encode(phoneNum, "UTF-8");
+
+        String text = "";
+        BufferedReader reader=null;
+
+        // Send data
+        try
+        {
+
+            // Defined URL  where to send data
+            URL url = new URL(restUrl);
+
+            // Send POST data request
+
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            wr.write( data );
+            wr.flush();
+
+            // Get the server response
+
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line = "";
+
+            // Read Server Response
+            while((line = reader.readLine()) != null)
+            {
+                // Append server response in string
+                sb.append(line + "\n");
+            }
+
+
+            text = sb.toString();
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+
+                reader.close();
+            }
+
+            catch(Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        // Show response on activity
+        return text;
+
+
+
+    }
+
+
+    public void btnAddOnClick(View view) throws UnsupportedEncodingException {
+        String restUrl = "http://10.0.2.2:80/mobileProject/addCustomer.php";
+        this.processRequest(restUrl);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.INTERNET},
+                    123);
+
+        } else{
+            /*SendPostRequest runner = new SendPostRequest();
+            runner.execute(restUrl);*/
+
+            Thread thread = new Thread(new MyTask2(restUrl));
+            thread.start();
+        }
+    }
+}
+
+class MyTask2 implements Runnable{
+    private String url;
+    public MyTask2(String url){
+        this.url = url;
+    }
+    @Override
+    public void run() {
 
     }
 }
