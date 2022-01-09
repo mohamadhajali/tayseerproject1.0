fa25bac4e1add02c06fbf386dac975bf28126f9f
mohamadhajali <mohamad184aliq@gmail.com>
2022-01-09 22:51:29 +0200

    1.3

--- app/src/main/java/com/example/androidproject/addnewcustomer.java
+++ app/src/main/java/com/example/androidproject/addnewcustomer.java
@@ -7,18 +7,24 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class addnewcustomer extends AppCompatActivity {
         EditText fullname;
@@ -37,11 +43,35 @@ public class addnewcustomer extends AppCompatActivity {
         phonenumber = findViewById(R.id.phonenumber);
         save = findViewById(R.id.savecustmer);
 
+
     }
+public void btnAddOnClick(View view)
+{
+
+    String url = "http://192.168.1.108:80/mobileProject/addCustomer.php";
+    if (ContextCompat.checkSelfPermission(this,
+            Manifest.permission.INTERNET)
+            != PackageManager.PERMISSION_GRANTED) {
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.INTERNET},
+                123);
+
+    } else{
+            SendPostRequest runner = new SendPostRequest();
+            runner.execute(url);
+
+      //  Thread thread = new Thread(new MyTask2(url));
+        //thread.start();
+    }
+}
+
+
     private String processRequest(String restUrl) throws UnsupportedEncodingException {
-        String userName  = username.getText().toString();
-        String fullName  = fullname.getText().toString();
-        String phoneNum  = phonenumber.getText().toString();
+        String userName = username.getText().toString();
+        String fullName = fullname.getText().toString();
+        String phoneNum = phonenumber.getText().toString();
+
 
         String data = URLEncoder.encode("userName", "UTF-8")
                 + "=" + URLEncoder.encode(userName, "UTF-8");
@@ -53,21 +83,24 @@ public class addnewcustomer extends AppCompatActivity {
                 + "=" + URLEncoder.encode(phoneNum, "UTF-8");
 
         String text = "";
-        BufferedReader reader=null;
+        BufferedReader reader = null;
 
         // Send data
-        try
-        {
+        try {
 
             // Defined URL  where to send data
             URL url = new URL(restUrl);
 
             // Send POST data request
 
-            URLConnection conn = url.openConnection();
+          //  URLConnection conn = url.openConnection();
+           // HttpsURLConnection httpsURLConnection = (HttpsURLConnection)url.openConnection();
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
             conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+
             OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-            wr.write( data );
+            wr.write(data);
             wr.flush();
 
             // Get the server response
@@ -77,68 +110,47 @@ public class addnewcustomer extends AppCompatActivity {
             String line = "";
 
             // Read Server Response
-            while((line = reader.readLine()) != null)
-            {
+            while ((line = reader.readLine()) != null) {
                 // Append server response in string
                 sb.append(line + "\n");
             }
 
 
             text = sb.toString();
-        }
-        catch(Exception ex)
-        {
+        } catch (Exception ex) {
             ex.printStackTrace();
-        }
-        finally
-        {
-            try
-            {
+        } finally {
+            try {
 
                 reader.close();
-            }
-
-            catch(Exception ex) {
+            } catch (Exception ex) {
                 ex.printStackTrace();
             }
         }
 
         // Show response on activity
+
+        System.out.println(text);
         return text;
 
 
-
     }
+    private class SendPostRequest extends AsyncTask<String ,Void,String>
+    {
 
-
-    public void btnAddOnClick(View view) throws UnsupportedEncodingException {
-        String restUrl = "http://10.0.2.2:80/mobileProject/addCustomer.php";
-        this.processRequest(restUrl);
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.INTERNET)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.INTERNET},
-                    123);
-
-        } else{
-            /*SendPostRequest runner = new SendPostRequest();
-            runner.execute(restUrl);*/
-
-            Thread thread = new Thread(new MyTask2(restUrl));
-            thread.start();
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return processRequest(urls[0]);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String result){
+            Toast.makeText(addnewcustomer.this,result,Toast.LENGTH_SHORT).show();
         }
     }
 }
 
-class MyTask2 implements Runnable{
-    private String url;
-    public MyTask2(String url){
-        this.url = url;
-    }
-    @Override
-    public void run() {
-
-    }
-}
