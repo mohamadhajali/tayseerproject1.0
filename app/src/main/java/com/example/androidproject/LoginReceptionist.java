e8b8cac74948f7e670a4d2ee14daf5c0629a0535
mohamadhajali <mohamad184aliq@gmail.com>
2022-01-10 17:25:49 +0200

    1.6

--- app/src/main/java/com/example/androidproject/LoginReceptionist.java
+++ app/src/main/java/com/example/androidproject/LoginReceptionist.java
@@ -1,28 +1,352 @@
 package com.example.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
 
 public class LoginReceptionist extends AppCompatActivity {
     Button login;
+    EditText resname;
+    EditText password;
+    private boolean CheckEditText;
+    ProgressDialog progressDialog;
+    HashMap<String, String> hashMap = new HashMap<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_receptionist);
         login();
     }
+
     private void login() {
+
+
         login = findViewById(R.id.loginbutton);
-        login.setOnClickListener(new View.OnClickListener() {
+       /* login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent =new Intent(LoginReceptionist.this, ReceptionistAdd.class);
                 startActivity(intent);
             }
-        });
+        });*/
+
+
+
+
+    }
+    public void btnAddOnClick(View view)
+    {
+
+        String url = "http://192.168.1.108:80/mobileProject/loginres.php";
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.INTERNET},
+                    123);
+
+        } else{
+            LoginReceptionist.SendPostRequest runner = new LoginReceptionist.SendPostRequest();
+            runner.execute(url);
+
+            //  Thread thread = new Thread(new MyTask2(url));
+            //thread.start();
+        }
+    }
+
+    private class SendPostRequest extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+
+                return processRequest(urls[0]);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if(result.equalsIgnoreCase("DataMatched")){
+                //Toast.makeText(LoginReceptionist.this, result, Toast.LENGTH_SHORT).show();
+                Intent intent =new Intent(LoginReceptionist.this, ReceptionistAdd.class);
+                startActivity(intent);
+            }
+            else {
+                System.out.println(result+"-kkkkkkkkkkkk");
+            }
+
+        }
+    }
+
+
+    private String processRequest(String restUrl) throws UnsupportedEncodingException {
+
+
+        resname = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        String rn = resname.getText().toString();
+        String rp = password.getText().toString();
+        String data = URLEncoder.encode("rn", "UTF-8")
+                + "=" + URLEncoder.encode(rn, "UTF-8");
+
+        data += "&" + URLEncoder.encode("rp", "UTF-8") + "="
+                + URLEncoder.encode(rp, "UTF-8");
+
+
+        String text = "";
+        BufferedReader reader = null;
+
+        // Send data
+        try {
+
+            // Defined URL  where to send data
+            URL url = new URL(restUrl);
+
+            // Send POST data request
+
+            //  URLConnection conn = url.openConnection();
+            // HttpsURLConnection httpsURLConnection = (HttpsURLConnection)url.openConnection();
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("GET");
+
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            wr.write(data);
+            wr.flush();
+
+            // Get the server response
+
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line = "";
+
+            // Read Server Response
+            while ((line = reader.readLine()) != null) {
+                // Append server response in string
+                sb.append(line + "\n");
+            }
+
+            text = sb.toString();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+
+                reader.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        return text;
+
     }
 }
+
+/*import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class LoginReceptionist extends AppCompatActivity {
+
+    // CONNECTION_TIMEOUT and READ_TIMEOUT are in milliseconds
+
+    public static final int CONNECTION_TIMEOUT=10000;
+    public static final int READ_TIMEOUT=15000;
+    private EditText etEmail;
+    private EditText etPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_receptionist);
+
+        // Get Reference to variables
+        etEmail = (EditText) findViewById(R.id.username);
+        etPassword = (EditText) findViewById(R.id.password);
+
+    }
+
+    // Triggers when LOGIN Button clicked
+    public void checkLogin(View arg0) {
+
+        // Get text from email and passord field
+        final String email = etEmail.getText().toString();
+        final String password = etPassword.getText().toString();
+
+        // Initialize  AsyncLogin() class with email and password
+        new AsyncLogin().execute(email,password);
+
+    }
+
+    private class AsyncLogin extends AsyncTask<String, String, String>
+    {
+        ProgressDialog pdLoading = new ProgressDialog(LoginReceptionist.this);
+        HttpURLConnection conn;
+        URL url = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            //this method will be running on UI thread
+            pdLoading.setMessage("\tLoading...");
+            pdLoading.setCancelable(false);
+            pdLoading.show();
+
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+
+                // Enter URL address where your php file resides
+                url = new URL("http://192.168.1.108:80/mobileProject/loginres.php");
+
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return "exception";
+            }
+            try {
+                // Setup HttpURLConnection class to send and receive data from php and mysql
+                conn = (HttpURLConnection)url.openConnection();
+                conn.setReadTimeout(READ_TIMEOUT);
+                conn.setConnectTimeout(CONNECTION_TIMEOUT);
+                conn.setRequestMethod("POST");
+
+                // setDoInput and setDoOutput method depict handling of both send and receive
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                // Append parameters to URL
+                Uri.Builder builder = new Uri.Builder()
+                        .appendQueryParameter("email", params[0])
+                        .appendQueryParameter("password", params[1]);
+                String query = builder.build().getEncodedQuery();
+
+                // Open connection for sending data
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                os.close();
+                conn.connect();
+
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+                return "exception";
+            }
+
+            try {
+
+                int response_code = conn.getResponseCode();
+
+                // Check if successful connection made
+                if (response_code == HttpURLConnection.HTTP_OK) {
+
+                    // Read data sent from server
+                    InputStream input = conn.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+
+                    // Pass data to onPostExecute method
+                    return(result.toString());
+
+                }else{
+
+                    return("unsuccessful");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "exception";
+            } finally {
+                conn.disconnect();
+            }
+
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            //this method will be running on UI thread
+
+            pdLoading.dismiss();
+
+            if(result.equalsIgnoreCase("Data Matched"))
+            {
+                /* Here launching another activity when login successful. If you persist login state
+                use sharedPreferences of Android. and logout button to clear sharedPreferences.
+
+               Intent intent =new Intent(LoginReceptionist.this, ReceptionistAdd.class);
+               startActivity(intent);
+System.out.println("DONE");
+            }else if (result.equalsIgnoreCase("Invalid Username or Password Please Try Again")){
+
+                // If username and password does not match display a error message
+                Toast.makeText(LoginReceptionist.this, "Invalid email or password", Toast.LENGTH_LONG).show();
+
+            } else if (result.equalsIgnoreCase("exception") || result.equalsIgnoreCase("unsuccessful")) {
+
+                Toast.makeText(LoginReceptionist.this, "OOPs! Something went wrong. Connection Problem.", Toast.LENGTH_LONG).show();
+
+            }
+        }
+
+    }
+}*/
