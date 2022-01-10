b6a428420ea3db85b23efbfa62f80f39e1af7c64
mohamadEid444 <Kjm172000>
2022-01-10 21:33:44 +0200

    1.7

--- app/src/main/java/com/example/androidproject/CustomerLogin.java
+++ app/src/main/java/com/example/androidproject/CustomerLogin.java
@@ -1,29 +1,129 @@
 package com.example.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.*;
+import java.net.*;
 
 public class CustomerLogin extends AppCompatActivity {
     Button login;
+    EditText usernameEdt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_login);
-        logincoustmer();
+        login = findViewById(R.id.loginBtn);
+        usernameEdt = findViewById(R.id.usernameEdt);
     }
 
-    private void logincoustmer() {
-        login = findViewById(R.id.logincous);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent =new Intent(CustomerLogin.this, CustomerRecycler.class);
-                startActivity(intent);
-            }
-        });
+    private void logInCustmer() {
+        Intent intent =new Intent(CustomerLogin.this, CustomerRecycler.class);
+        startActivity(intent);
     }
+
+    public void btnLoginClick(View view) {
+        String url = "http://192.168.1.11:80/mobileProject/login.php?userName="+usernameEdt.getText();
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.INTERNET},
+                    123);
+
+        } else{
+            LoginTask log = new LoginTask();
+            log.execute(url);
+
+        }
+    }
+
+    private InputStream OpenHttpConnection(String urlString) throws IOException
+    {
+        InputStream in = null;
+        int response = -1;
+
+        URL url = new URL(urlString);
+        URLConnection conn = url.openConnection();
+
+        if (!(conn instanceof HttpURLConnection))
+            throw new IOException("Not an HTTP connection");
+        try{
+            HttpURLConnection httpConn = (HttpURLConnection) conn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setInstanceFollowRedirects(true);
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+            response = httpConn.getResponseCode();
+            if (response == HttpURLConnection.HTTP_OK) {
+                in = httpConn.getInputStream();
+            }
+        }
+        catch (Exception ex)
+        {
+            Log.d("Networking", ex.getLocalizedMessage());
+            throw new IOException("Error connecting");
+        }
+        return in;
+    }
+    private String DownloadText(String URL)
+    {
+        int BUFFER_SIZE = 2000;
+        InputStream in = null;
+        try {
+            in = OpenHttpConnection(URL);
+        } catch (IOException e) {
+            Log.d("Networking", e.getLocalizedMessage());
+            return "";
+        }
+
+        InputStreamReader isr = new InputStreamReader(in);
+        int charRead;
+        String str = "";
+        char[] inputBuffer = new char[BUFFER_SIZE];
+        try {
+            while ((charRead = isr.read(inputBuffer))>0) {
+                //---convert the chars to a String---
+                String readString =
+                        String.copyValueOf(inputBuffer, 0, charRead);
+                str += readString;
+                inputBuffer = new char[BUFFER_SIZE];
+            }
+            in.close();
+        } catch (IOException e) {
+            Log.d("Networking", e.getLocalizedMessage());
+            return "";
+        }
+        return str;
+    }
+
+
+    private class LoginTask extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return DownloadText(strings[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(CustomerLogin.this,result,Toast.LENGTH_SHORT).show();
+            if(result.equals("Log in successfully"))
+                logInCustmer();
+        }
+    }
+
 }
